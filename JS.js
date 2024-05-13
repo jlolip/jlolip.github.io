4c4e28cb40ea4aa77ec5914d95f8e2d25774902b
jlolip <hiphopnikit@gmail.com>
2024-05-13 19:39:20 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,3 +1,6 @@
+var mb=tg.MainButton;
+mb.text="Надіслати";
+mb.show();
 //documet.getElementById('people_name').innerHtml=window.Telegram.WebApp.initDataUnsafe.user.id;
 document.getElementById('people_name').innerHtml+=window.Telegram.WebApp.initDataUnsafe.user.first_name+" ";
 document.getElementById('people_name').innerHtml+=window.Telegram.WebApp.initDataUnsafe.user.last_name+" ";
