b33a61ef201545ec042e5c813bce5e05ac3333cc
jlolip <hiphopnikit@gmail.com>
2024-05-13 19:27:06 +0300

    Update JS.js

---
+++ JS.js
@@ -0,0 +1,3 @@
+//documet.getElementById('people_name').innerHtml=window.Telegram.WebApp.initDataUnsafe.user.id;
+documet.getElementById('people_name').innerHtml+=window.Telegram.WebApp.initDataUnsafe.user.first_name+" ";
+documet.getElementById('people_name').innerHtml+=window.Telegram.WebApp.initDataUnsafe.user.last_name+" ";
