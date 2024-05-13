68ec8fa8d071579612afe6dc83d29d7ad449568e
jlolip <hiphopnikit@gmail.com>
2024-05-13 19:31:22 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,3 +1,3 @@
 //documet.getElementById('people_name').innerHtml=window.Telegram.WebApp.initDataUnsafe.user.id;
-documet.getElementById('people_name').innerHtml+=window.Telegram.WebApp.initDataUnsafe.user.first_name+" ";
-documet.getElementById('people_name').innerHtml+=window.Telegram.WebApp.initDataUnsafe.user.last_name+" ";
+document.getElementById('people_name').innerHtml+=window.Telegram.WebApp.initDataUnsafe.user.first_name+" ";
+document.getElementById('people_name').innerHtml+=window.Telegram.WebApp.initDataUnsafe.user.last_name+" ";
